322c70f3548802985b8fe1a47e0592cf590dc2d5
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-08-28 14:52:44 +0200

    Update familytreemaker.sh
    bugfix siblingcode

--- familytreemaker.sh
+++ familytreemaker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+h
 
 #MIT License
 #
@@ -101,190 +101,14 @@ while [ $i -le ${lines%%$1} ]; do
 	# Here we apply the settings for the household box, making it invisible
 	echo "$household[shape=circle,label=\"\",height=0.01,width=0.01];"
     else #The person is a Child in this section
-	string=""
+	siblings=""
 	# Continue down finding the persons siblings until we hit a new household
 	while [[ "$raw" = "    "* ]]; do
 	    # Parse the line so that it includes just the name of the person
 	    line=${raw#    }
 	    line=${line%% (*}
-	    # Add the child to $string which will c#!/bin/bash
-2
-​
-3
-#MIT License
-4
-#
-5
-#Copyright (c) 2021 user18130814200115-2
-6
-#
-7
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-8
-#of this software and associated documentation files (the "Software"), to deal
-9
-#in the Software without restriction, including without limitation the rights
-10
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-11
-#copies of the Software, and to permit persons to whom the Software is
-12
-#furnished to do so, subject to the following conditions:
-13
-#
-14
-#The above copyright notice and this permission notice shall be included in all
-15
-#copies or substantial portions of the Software.
-16
-#
-17
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-18
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-19
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-20
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-21
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-22
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-23
-#SOFTWARE.
-24
-​
-25
-# Get the number of lines of the .tree file
-26
-lines=$(wc -l "$1")
-27
-​
-28
-# Initial settings for DOT, box shaped and no arrows
-29
-echo "digraph { node [shape=box];edge [dir=none];"
-30
-​
-31
-i=1
-32
-# First we loop over all lines to make setting for all persons in the tree
-33
-while [ $i -le ${lines%%$1} ]; do
-34
-    # Reset some variables
-35
-    sex=""
-36#!/bin/bash
-2
-​
-3
-#MIT License
-4
-#
-5
-#Copyright (c) 2021 user18130814200115-2
-6
-#
-7
-#Permission is hereby granted, free of charge, to any person obtaining a copy
-8
-#of this software and associated documentation files (the "Software"), to deal
-9
-#in the Software without restriction, including without limitation the rights
-10
-#to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-11
-#copies of the Software, and to permit persons to whom the Software is
-12
-#furnished to do so, subject to the following conditions:
-13
-#
-14
-#The above copyright notice and this permission notice shall be included in all
-15
-#copies or substantial portions of the Software.
-16
-#
-17
-#THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-18
-#IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-19
-#FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-20
-#AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-21
-#LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-22
-#OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-23
-#SOFTWARE.
-24
-​
-25
-# Get the number of lines of the .tree file
-26
-lines=$(wc -l "$1")
-27
-​
-28
-# Initial settings for DOT, box shaped and no arrows
-29
-echo "digraph { node [shape=box];edge [dir=none];"
-30
-​
-31
-i=1
-32
-# First we loop over all lines to make setting for all persons in the tree
-33
-while [ $i -le ${lines%%$1} ]; do
-34
-    # Reset some variables
-35
-    sex=""
-36
-    year=""
-37
-    name=""
-38
-    # Get the contents of the line we are working on
-39
-    raw=$(pl "$i" "$1")
-40
-    # Parse the line so that it includes just the name of the person
-41
-    id=${raw#    }
-42
-    id=${id%% (*}
-43
-    # Arguments are added in brackets at the end of the line
-44
-    arguments=${raw#*(}
-45
-    arguments=${arguments%%)}
-    year=""
-37
-    name=""
-38
-    # Get the contents of the line we are working on
-39
-    raw=$(pl "$i" "$1")
-40
-    # Parse the line so that it includes just the name of the person
-41
-    id=${raw#    }
-42
-    id=${id%% (*}
-43
-    # Arguments are added in brackets at the end of the line
-44
-    arguments=${raw#*(}
-45
-    arguments=${arguments%%)}ontain all the children
-	    string="$string\"$line\" -> "
+	    # Add the child to $string which will contain all the children
+	    siblings="$siblings\"$line\" -> "
 	    # Draw a line from the household generated before to the Child
 	    echo "$household -> \"$line\""	
 	    # Move to the next line and get it's contents
@@ -295,7 +119,7 @@ while [ $i -le ${lines%%$1} ]; do
 	i=$(expr "$i" - 1)
 	# Draw invisible lines between all the siblings removing the arrow at the end
 	# The grep statement is there so that this process is only done if the person has siblings, if we do not do this, DOT will get confused and erase the child
-	echo -e "{rank=same\n${string%->*}[style=invis]\n}" | grep "\-\>"
+	echo -e "{rank=same\n${siblings%->*}[style=invis]\n}" | grep -B1 -A1 "\->"
     fi
 
     # Move to the next line
