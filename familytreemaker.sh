5682715d5a8c78c4d365f885539980808f02f845
user18130814200115-2 <66143190+user18130814200115-2@users.noreply.github.com>
2021-08-31 12:25:36 +0200

    Update familytreemaker.sh
    (Posix) Rewrite
    - Paralell processing
    - fewer dependencies
    - faster compile time

--- familytreemaker.sh
+++ familytreemaker.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 
 #MIT License
 #
@@ -22,111 +22,76 @@
 #OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 #SOFTWARE.
 
-# Get the number of lines of the .tree file
-lines=$(wc -l "$1")
+file="$1"
 
-# Initial settings for DOT, box shaped and no arrows
-echo "digraph {	node [shape=box];edge [dir=none];rankdir=LR;constraint=false"
+GetArguments(){
+    sed "$file" -e 's/[][ ]//g' -e 's/(.*)//g' | tr '\n' ';'
+    sed "$file" -e 's/^    //g'\
+	-e 's/[][]//g'\
+	-e 's/(F/[style=filled;fillcolor=bisque;/'\
+	-e 's/(M/[style=filled;fillcolor=azure2;/'\
+	-e 's/,/label=/g' -e 's/;)/;label=)/' |\
+	sed -E 's/^([^[]*)(.*)label=(.*)\)/\1\2label=\1\\n\3]/g' |\
+	sed -E 's/label=(.*)label=.*\\n([^]]*)]/label=\2\\n\1]/' |\
+	sed -e 's/;)/]/' -e 's/[^]]$/&[]/' -e :a -e '/ .*\[/s/ //;ta' |\
+	sed -E 's/label=([^]]*)/label=\"\1\"/'
+}
 
-i=1
-list=""
-# First we loop over all lines to make setting for all persons in the tree
-while [ $i -le ${lines%%$1} ]; do
-    # Reset some variables
-    sex=""
-    year=""
-    name=""
-    # Get the contents of the line we are working on
-    raw=$(pl "$i" "$1")
-    # Parse the line so that it includes just the name of the person
-    id=${raw#    }
-    id=${id%% (*}
-    # Arguments are added in brackets at the end of the line
-    arguments=${raw#*(}
-    arguments=${arguments%%)}
-    # Split the arguments at the comma
-    IFS=',' read -ra argarr <<< "$arguments"
-    # Get the number of arguments 
-    [ -z "$arguments" ] && n=0 || n="${#argarr[@]}"
-    # Arguments are (in order) sex, birthyear and name
-    [ $n -gt 0 ] && sex=${argarr[0]}
-    [ $n -gt 1 ] && year=${argarr[1]}
-    # If the name is not set, use the id instead
-    [ $n -gt 2 ] && name=${argarr[2]} || name=$id
-    # Print everyone with their $name, $birthyear and...
-    # If the person is designated as Male, colour their box blue
-    if [ "$sex" = "M" ]
-    then
-	echo "\"$id\"[style=filled,fillcolor=azure2];"
-    # If they are female colour them orange
-    elif [ "$sex" = "F" ]
-    then
-	echo "\"$id\"[style=filled,fillcolor=bisque];"
-    fi
-    echo "\"$id\"[label=\"$name\n$year\"];"
-    i=$(expr "$i" + 1)
-done
+GetHouseholds() {
+    # We do a multi-line grep (with pcregrep) on the file
+    # This will output all member of a household (parents and children) followed by the string [HOUSEHOLD]
+    rawgrep=$(pcregrep -Mo "^[A-z].*\n^[A-z].*\n(^    .*\n)*" $file)
+    households=$(printf "%s\n\n" "$rawgrep" | sed 's/[][]//g' | sed 's/^$/[HOUSEHOLD]/g' | sed 's/([^)]*)//g' | sed 's/ //g')
+    regex=$(printf "%s\n\n" "$rawgrep" | sed 's/^$/[HOUSEHOLD]/g' | grep "^    \\|\[HOUSEHOLD\]" | sed 's/[ ]*([^)]*)//' | sed 's/[A-z]$/&\\\\|/' | sed 's/^    /\^/' | tr -d "\n" | sed 's/\[HOUSEHOLD\]\\\\|/\n/g' | sed 's/\\\\|$//')
+    index=0
+    printf "%s;" $households | sed 's/\[HOUSEHOLD\]/\n/g' | { while read household; do
+	lineindex=$((index +1))
+	HandleHousehold "${household#;}" "$index" "$(printf "%s" "$regex" | sed "${lineindex}q;d")"
+	index=$((index + 1))
+    done
+    wait
+    }
+}
 
-# Now we enter a new loop to generate the tree
-i=1
-while [ $i -le ${lines%%$1} ]; do
-    # Get the contents of the line we are working on
-    raw=$(pl "$i" "$1")
-    # Parse the line so that it includes just the name of the person
-    line=${raw#    }
-    line=${line%% (*}
-
-    # If we do not start with four spaces, then we are designating a household
-    if [[ "$raw" != "    "* ]]
-    then
-	# The next line will designate the partner
-	i=$(expr "$i" + 1)
-	line2=$(pl "$i" "$1")
-	line2=${line2#    }
-	line2=${line2%% (*}
-	# Each household is given a unique ID.
-	# This id will get it's own (invisible) box in the graph so that parents come together and then flow to their children instead of both having lines to their respective children
-	household="h$i"
-	# Make sure the partners and the household are graphed at the same height
-	echo -e "{rank=\"same\"\n\"$line\" $household \"$line2\"\n}"
-	# Now tell DOT to draw a line from member1 to the household to member2
-	echo "\"$line\" -> $household[weight=10]"
-	echo " $household -> \"$line2\"[weight=10]"
-	# Here we apply the settings for the household box, making it invisible
-	echo "$household[shape=circle,label=\"\",height=0.01,width=0.01];"
-    else #The person is a Child in this section
-	siblings=""
-	count=0
-	# Continue down finding the persons siblings until we hit a new household
-	while [[ "$raw" = "    "* ]]; do
-	    # Parse the line so that it includes just the name of the person
-	    line=${raw#    }
-	    line=${line%% (*}
-	    # Here we make a connector for the given child which will connect later to the household union of the parents. Doing this keeps the lines straight.
-	    siblings="$siblings\"$household $i\" -> "
-	    echo "\"$household $i\"[shape=circle,label=\"\",height=0.01,width=0.01];"
-	    # Count the number of children
-	    count=$(expr "$count" + 1)
-	    # Draw a line from the connector generated before to the Child
-	    echo "\"$household $i\" -> \"$line\"[weight=15]"	
-	    # Move to the next line and get it's contents
-	    i=$(expr "$i" + 1)
-	    raw=$(pl "$i" "$1")
-	done
-	# Calculate to which connector we should connect the household
-	n=$(expr "$i" - $(expr "$count" / 2) - 1)
-	# Move back up one line. Since we finish the loop once we hit a new household and we move down one person before the loop restarts
-	i=$(expr "$i" - 1)
-	# If there are any kids, connect the household to the connector
-	[ $count -gt 0 ] && echo "$household -> \"$household $n\""
-	# Depending on your family, you may get better results if you change the weight from -1 to 1, this keeps siblings closer together.
-	# Try this if your family members did not have many kids.
-	echo "{rank=same; ${siblings%%-> }[weight=-1]}"
-    fi
-
-    # Move to the next line
-    i=$(expr "$i" + 1)
-done
-
-# close the diagraph
-echo "}"
+HandleHousehold() {
+    # Parents
+    h=h
+    div=sub
+    parents="{rank=same;\
+$(printf "%s\n" "$1" |\
+cut -d ';' -f '1,2' --output-delimiter " -> h$2 -> ")\
+[weight=10]}\nh$2[shape=point;width=0;height=0]\n"
+    # Get The children
+    children=$(printf "%s\n" "$1" | cut -d ";" -f "3-" --output-delimiter "\n")
+    # Loop over the children
+    printf "%b" $children | { index=0; while read child; do
+	#Sub-Household-Settings
+	shs="$shs$h$2$div$index[shape=point;width=0;height=0]\n"
+	#Edges between Sub-Households
+	sh="$sh$h$2$div$index -> "
+	#Edges from Sub-Household to Corresponding Child
+	sh2c="$sh2c$h$2$div$index -> $child[weight=15]\n"
+	index=$((index + 1))
+    done
+    # Calculate to which sub-household we connect the parent's household 
+    [ -z "$children" ] || h2sh="h$2 -> $h$2$div$((index /2))\n"
+    # Check if the kids have partners (to determine weight)
+    [ $(printf "grep -c \"%s\" $file\n" "$3" | sh) -eq 0 ] && weight=1 || weight=-1
+    # Make sure all the sub-households are vertically aligned AND that there is not trailing ->
+    [ $index -gt 1 ] && sh="{rank=same;${sh% -> }[weight=$weight]}\n" || sh=""
+    # Now print the whole thing
+    #This done instead of printing at each step to allow for parallel processing
+    printf "$parents$shs$sh$sh2c$h2sh"
+    }
+}
+GetFloating() {
+    raw=$(tail -n1 "$file" | grep "^#")
+    printf "\n${raw#\#}"
+}
+# Start the Graph
+one=$(GetArguments &)
+two=$(GetHouseholds &)
+three=$(GetFloating)
+wait
+# Print the graph
+printf "digraph {node [shape=box];edge [dir=none];rankdir=LR;constraint=false\n%s}\n" "$one$two$three"
